903dbdf3b78f1435c84ca484e112793e4ed87d41
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-12-08 02:11:08 -0800

    github and linkedin icon added to footer

--- src/components/footer/Copyright.jsx
+++ src/components/footer/Copyright.jsx
@@ -8,6 +8,21 @@ function Copyright() {
       <footer>
         {/* <p>Built with ❤️ in California</p> */}
         <p>Copyright © {date} | All rights reserved.</p>
+        <a href="https://github.com/scottdialo">
+          <img
+            src="github-mark.png"
+            className="github-icon"
+            Alt="github-icon"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/scottdialo/">
+        <img
+            src="LI-Logo.png"
+            className="linkedin-icon"
+            Alt="linkedin-icon"
+          />
+         
+        </a>
       </footer>
     </div>
   );
